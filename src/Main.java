3679b2bb5139f11eaac565ef0c104e8977d86db0
safdarahmed02 <safdarahmed9999@gmail.com>
2025-01-25 02:14:49 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\tMy name is Safdar Ahmed\n"); //one tab space, new line
+        System.out.println("\"I like learning computer science concepts.\"");//adding quotes
+
+        //This is a comment
+
+        /* This is a
+        multi-line comment
+         */
+
+        int x; //declaration
+        x = 4; //assignment
+
+        int y = 12;
+
+        System.out.println("My number is "+ y);
+    }
+}
---
+++ src/SwapVariables.java
@@ -0,0 +1,10 @@
+public class SwapVariables {
+    public static void main(String[] args) {
+        String x = "Jeera";
+        String y = "Soda";
+        String temp;
+
+        //actual swapping
+        
+    }
+}
