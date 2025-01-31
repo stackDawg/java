607909e6bb7c85063e7e61cabb86802dc9840bed
stackDawg <safdarsells@gmail.com>
2025-01-31 23:29:45 +0530

    added math class

---
+++ src/MathClass.java
@@ -0,0 +1,23 @@
+public class MathClass {
+    public static void main(String[] args) {
+
+/*
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+*/
+
+        double result;
+
+//        result = Math.pow(2,6);
+//        result = Math.abs(-5);
+//        result = Math.sqrt(49);
+//        result = Math.round(3.77);
+//        result = Math.ceil(2.1);
+//        result = Math.floor(2.8);
+//        result = Math.max(4,9);
+        result = Math.min(4,9);
+
+        System.out.println(result);
+
+    }
+}
