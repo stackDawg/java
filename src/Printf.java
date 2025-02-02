74ba9ce64ee63b690cdb51d2a68e9db563c10a8b
stackDawg <safdarsells@gmail.com>
2025-02-02 23:59:35 +0530

    new class printf

---
+++ src/Printf.java
@@ -0,0 +1,15 @@
+public class Printf {
+    public static void main(String[] args) {
+
+        String name = "John";
+        int age = 34;
+        char first_letter = 'J';
+        double height = 1.8;
+        boolean isEmployed = true;
+
+        System.out.printf("Hello %s", name);
+        System.out.printf("You are %d years old", age);
+
+
+    }
+}
